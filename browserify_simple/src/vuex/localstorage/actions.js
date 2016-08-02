b5a22c036cc0765984e54e1681f381469a2b4a84
toybox.yamaguchi <toybox.yamaguchi@1pac.jp>
2016-08-02 12:15:40 +0900

    project file

--- browserify_simple/src/vuex/localstorage/actions.js
+++
@@ -1,6 +0,0 @@
-import * as types from '../mutation-types'
-
-// 元から来た引数の dispatch フィールドを取得するオブジェクト分割代入
-export const loadLocalStorage = ({ dispatch }, data) => {
-  dispatch(types.SAVE_LOCALSTORAGE, data)
-}
--- browserify_simple/src/vuex/timeline/getters.js
+++
@@ -1,5 +0,0 @@
-// 別のステートのスコープからアクセスされる可能性があるので state.timeline.all でアクセス
-
-// export const timeline = state => {
-//   return state.timeline.all.reverse()
-// }
